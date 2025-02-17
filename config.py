f3a2e1aae2462a5cf69a3c3b9f5ac313f1ca621d
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-02-17 20:52:51 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+class Config: 
+  TELEGRAM_BOT_TOKEN = 'AAPKA_TELEGRAM_BOT_TOKEN' 
+  TELEGRAM_API_ID = 'AAPKA_TELEGRAM_API_ID' 
+  CHANNEL_USERNAME = 'aapka_channel_username' 
+  POST_ID = 'post_id' 
+  PROXY_LIST_FILE = 'proxies.txt' 
