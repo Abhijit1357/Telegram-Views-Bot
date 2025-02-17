fe97c7c68a31f223a591003b954416afa851aa65
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-02-17 21:11:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,10 @@
-class Config: 
-  TELEGRAM_BOT_TOKEN = 'AAPKA_TELEGRAM_BOT_TOKEN' 
-  TELEGRAM_API_ID = 'AAPKA_TELEGRAM_API_ID' 
-  CHANNEL_USERNAME = 'aapka_channel_username' 
-  POST_ID = 'post_id' 
-  PROXY_LIST_FILE = 'proxies.txt' 
+class Config:
+  TELEGRAM_BOT_TOKEN = '7729465278:AAFUIIZrAi0MTOrBmxN6yktOa4Nr4FdZiVE'
+  TELEGRAM_API_ID = 22834593
+  TELEGRAM_API_HASH = 'f400bc1d1baeb9ae93014ce3ee5ea835'
+  CHANNEL_USERNAME = '@Abt_Abhi'  
+  POST_ID = '-1002485988122'  
+  PROXY_LIST_FILE = 'proxies.txt'
+  MAX_VIEWS_PER_INTERVAL = 1000000  # views sending limit per interval increased to 1 million
+  VIEWS_SENDING_INTERVAL = 3600  # views sending interval increased to 1 hour (3600 seconds)
+  THREADS_COUNT = 500  # added new config for multiple threads (optional but recommended)
