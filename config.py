a140a2c53ab2133cca2e358b626e372e7efa0fb5
Abhijit1357 <165138363+Abhijit1357@users.noreply.github.com>
2025-02-18 08:28:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,20 @@
 class Config:
-  TELEGRAM_BOT_TOKEN = '7729465278:AAFUIIZrAi0MTOrBmxN6yktOa4Nr4FdZiVE'
-  TELEGRAM_API_ID = 22834593
-  TELEGRAM_API_HASH = 'f400bc1d1baeb9ae93014ce3ee5ea835'
-  CHANNEL_USERNAME = '@Abt_Abhi'  
-  POST_ID = '-1002485988122'  
-  PROXY_LIST_FILE = 'proxies.txt'
-  MAX_VIEWS_PER_INTERVAL = 1000000  # views sending limit per interval increased to 1 million
-  VIEWS_SENDING_INTERVAL = 3600  # views sending interval increased to 1 hour (3600 seconds)
-  THREADS_COUNT = 500  # added new config for multiple threads (optional but recommended)
+    TELEGRAM_BOT_TOKEN = '7729465278:AAFUIIZrAi0MTOrBmxN6yktOa4Nr4FdZiVE'  # Bot Token
+    TELEGRAM_API_ID = 22834593  # Telegram API ID
+    TELEGRAM_API_HASH = 'f400bc1d1baeb9ae93014ce3ee5ea835'  # Telegram API Hash
+    CHANNEL_USERNAME = '@Abt_Abhi'  # Channel Username
+    POST_ID = '-1002485988122'  # Post ID
+    PROXY_LIST_FILE = 'proxies.txt'  # Proxy List File
+    MAX_VIEWS_PER_INTERVAL = 1000000  # Views sending limit per interval
+    VIEWS_SENDING_INTERVAL = 3600  # Views sending interval
+    THREADS_COUNT = 500  # Multiple threads count
+    LOGGER_ID = -1002134425165  # Log Group ID
+
+    # Optional config
+    # API_ID aur API_HASH ko my.telegram.org se prapt karein
+    # CHANNEL_USERNAME ko apne channel ka username daalein
+    # POST_ID ko apne post ka ID daalein
+    # PROXY_LIST_FILE ko apne proxies.txt file ka naam daalein
+    # MAX_VIEWS_PER_INTERVAL aur VIEWS_SENDING_INTERVAL ko apni zaroorat ke hisab se set karein
+    # THREADS_COUNT ko apni zaroorat ke hisab se set karein
+    # LOGGER_ID ko apne log group ka ID daalein
